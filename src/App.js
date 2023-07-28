9525c0569c895c1172e120b0096afd3f1ba44f86
swapnil2165 <swapnilchikanepatil2165@gmail.com>
2023-07-28 16:05:28 +0530

    message

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Task2A from "./Task2A/Main"
+
+function App() {
+  return (
+    <>
+      <Task2A/>
+    </>
+  );
+}
+
+export default App;
